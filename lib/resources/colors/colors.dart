4ab3737dfe5588bed4a572eca6bad77fea1de3eb
PatelPruthvi <pruthvikp8613@gmail.com>
2024-11-16 10:07:56 +0530

    Updated assets, colors and dependencies.

--- lib/resources/colors/colors.dart
+++ lib/resources/colors/colors.dart
@@ -7,6 +7,8 @@ class AppColors {
   static const Color kGreen = Color.fromARGB(255, 29, 164, 33);
   static const Color kDarkGreen = Color.fromARGB(255, 44, 132, 47);
   static const Color kMaroon = Color.fromARGB(255, 158, 21, 11);
+  static const Color kLightMaroon = Color.fromARGB(255, 132, 36, 29);
+  static const Color kYellow = Colors.yellow;
   static const Color kBlue = Colors.blue;
   static const Color kLightGray = Colors.grey;
   static Color? kDarkGray = Colors.grey[800];
