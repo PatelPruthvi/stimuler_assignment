57c74fc093d227ba59e09f717be8207815bf711a
PatelPruthvi <pruthvikp8613@gmail.com>
2024-11-16 23:40:12 +0530

    Updated day_model.dart
    The days model will now include isCompleted

--- lib/models/day_model.dart
+++ lib/models/day_model.dart
@@ -4,16 +4,22 @@ import 'package:stimuler_assignment/models/exersice_model.dart';
 class DayModel {
   String title;
   List<Exersice> exersices;
-  DayModel({required this.title, required this.exersices});
+  bool isCompleted;
+  DayModel(
+      {required this.title,
+      required this.exersices,
+      required this.isCompleted});
 
   factory DayModel.fromJson(Map<String, dynamic> json) => DayModel(
       title: json["title"],
       exersices: List<Exersice>.from(
-          json["exersices"].map((x) => Exersice.fromJson(x))));
+          json["exersices"].map((x) => Exersice.fromJson(x))),
+      isCompleted: json['isCompleted']);
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data["title"] = title;
     data["exersices"] = List<dynamic>.from(exersices.map((x) => x.toJson()));
+    data["isCompleted"] = isCompleted;
 
     return data;
   }
